d27039f3a778bcc2d65af3dea2fc32d50b501da4
Adrienne McDonnell <amcdonnell@delphicdigital.com>
2016-11-22 08:57:37 -0500

    add all aria attr to accordion component

--- Static/src/js/components/accordion.js
+++ Static/src/js/components/accordion.js
@@ -5,36 +5,30 @@ define(['jquery'],function($) {
 		settings: {
 			$accordion: $('.accordion'),
 			$tab: $('.accordion__tab'),
-			$panel: $('.accordion__panel'),
-			currentContext: MQ.getContext()
+			$panel: $('.accordion__panel')
 		},
 
 		init: function() {
 			var _ = this;
-			var $this = $(this);
 
-			//get current onmediquery context
-			var currentContext = MQ.getContext();
-
-			if (currentContext === "mobile") {
-				_.initMobile();
-			} else {
-				_.initTabletDesktop();
-			}
-
-			this.bindAriaEvents();
+			_.bindAriaEvents();
 			_.initClickEvent();
 		},
 
 		initClickEvent: function() {
 			var _ = this;
-			var $this = $(this);
 
 			_.settings.$tab.off().on('click keypress', function(e) {
-				$(this).next('.accordion__panel').slideToggle(300);
-				$(this).children('.accordion__icon').toggleClass('open');
+				$clickedTab = $(this);
+				$clickedTab.next('.accordion__panel').slideToggle(300);
+				$clickedTab.children('.accordion__icon').toggleClass('open');
 
-				// _.setAriaVisible($this);
+				if ( $clickedTab.children('.accordion__icon').hasClass('open') ){
+					_.setAriaVisible($clickedTab);
+				} else {
+					_.setAriaHidden($clickedTab);
+				}
+				
   			});
 		},
 
@@ -43,31 +37,13 @@ define(['jquery'],function($) {
 
 			$(_.settings.$accordion).each(function(i){
 				var $this = $(this);
+				$this.attr('id', 'accordion_' + i);
 
-				_.setDefaultAriaAttributes($this);
+				_.setDefaultAriaAttributes($this, i);
 			});
-
-			// $(_.settings.$mainNavItem).each(function(i){
-			// 	var $this = $(this);
-
-			// 	_.setDefaultAriaAttributes($this);
-			// });
-
-			// _.settings.$mainNavItem.mouseenter(function(e) {
-			// 	var $this = $(this);
-			// 		$flyout = $this.find(_.settings.$mainFlyout);
-
-			// 	_.setAriaVisible($this);
-
-			// }).mouseleave(function(e) {
-			// 	var $this = $(this);
-
-			// 	_.setAriaHidden($this);
-
-			// });
 		},
 
-		setDefaultAriaAttributes: function($this) {
+		setDefaultAriaAttributes: function($this, i) {
 			var _ = this;
 
 			_.settings.$accordion
@@ -76,43 +52,39 @@ define(['jquery'],function($) {
 					'multiselectable': 'true'
 			});
 
-			$this.find(_.settings.$tab)
-				.attr({
+			$this.find(_.settings.$tab).each(function(j){
+				var $this = $(this);
+				$this.attr({
+					'id': 'a' + i + '_tab' + j,
 					'role': 'tab',
 					'tabindex': '0',
-					'aria-expanded': 'false'
-			});
-
-			$this.find(_.settings.$panel)
-				.attr({
+					'aria-expanded': 'false',
+					'aria-controls': 'a' + i + '_panel' + j
+				});
+			})
+				
+			$this.find(_.settings.$panel).each(function(j){
+				var $this = $(this);
+				$this.attr({
+					'id': 'a' + i + '_panel' + j,
 					'role': 'tabpanel',
-					'aria-hidden': 'true'
-			});
+					'aria-hidden': 'true',
+					'aria-labelledby': 'a' + i + '_tab' + j
+				});
+			})
+				
 		},
 
-		setAriaVisible: function($this) {
-			var _ = this;
-
-			$this.find('[aria-expanded=false]').attr('aria-expanded', 'true');
-			$this.find('[aria-hidden=true]').attr('aria-hidden', 'false');
-
-		},
-
-		setAriaHidden: function($this) {
-			var _ = this;
-
-			$this.find('[aria-expanded=true]').attr('aria-expanded', 'false');
-			$this.find('[aria-hidden=false]').attr('aria-hidden', 'true');
+		setAriaVisible: function($clickedTab) {
+			$clickedTab.attr('aria-expanded', 'true');
+			$clickedTab.next('[aria-hidden=true]').attr('aria-hidden', 'false');
 
 		},
 
-		initMobile: function() {
-			$('.accordion__panel').css('display', 'none');
-			$('.js-accordion-toggle').removeClass('open');
-		},
+		setAriaHidden: function($clickedTab) {
+			$clickedTab.attr('aria-expanded', 'false');
+			$clickedTab.next('[aria-hidden=false]').attr('aria-hidden', 'true');
 
-		initTabletDesktop: function() {
-	  	
 		},
 
 		destroy: function() { }
